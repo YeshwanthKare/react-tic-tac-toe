3c8a003e34aa2834b7386f1d4cbe1b7e26e37dab
YeshwanthKare <yesh.kare@gmail.com>
2021-08-02 00:20:34 +0530

    ADD: transform state to game history

--- src/App.js
+++ src/App.js
@@ -4,42 +4,53 @@ import { calculateWinner } from "./helper";
 import "./styles/root.scss";
 
 const App = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [isXNext, setIsXNext] = useState(false);
+  const [history, setHistory] = useState([
+    { board: Array(9).fill(null), isXNext: true },
+  ]);
+
+  const [currentMove, setCurrentMove] = useState(0);
+
+  // const [isXNext, setIsXNext] = useState(false);
   // console.log(board);
 
-  const handleSquareClick = (position) => {
-    if (board[position] || winner) {
-      return;
-    }
-    // console.log(setBoard);
+  const current = history[currentMove];
 
-    setBoard((prev) => {
-      // console.log("current board state", prev);
-      const newValue = prev.map((square, pos) => {
-        if (pos === position) {
-          return isXNext ? "X" : "O";
-        }
-        return square;
-      });
-      // console.log("this will be new board", newValue);
-      return newValue;
-    });
-    setIsXNext((prev) => (prev = !prev));
-  };
-
-  const winner = calculateWinner(board);
+  const winner = calculateWinner(current.board);
   console.log(winner);
 
   const message = winner
     ? `Winner is ${winner}`
-    : `Next Player is ${isXNext ? "X" : "O"}`;
+    : `Next Player is ${current.isXNext ? "X" : "O"}`;
+
+  const handleSquareClick = (position) => {
+    if (current.board[position] || winner) {
+      return;
+    }
+    // console.log(setBoard);
+
+    setHistory((prev) => {
+      // console.log("current board state", prev);
+
+      const last = prev[prev.length - 1];
+
+      const newValue = last.board.map((square, pos) => {
+        if (pos === position) {
+          return last.isXNext ? "X" : "O";
+        }
+        return square;
+      });
+      // console.log("this will be new board", newValue);
+      return prev.concat({ board: newValue, isXNext: !last.isXNext });
+    });
+    setCurrentMove((prev) => prev + 1);
+    // setIsXNext((prev) => (prev = !prev));
+  };
 
   return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
       <h2>{message}</h2>
-      <Board board={board} handleSquareClick={handleSquareClick} />
+      <Board board={current.board} handleSquareClick={handleSquareClick} />
     </div>
   );
 };
